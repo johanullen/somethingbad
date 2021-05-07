6236c1a8c77b2a21fa6994fbffb2ecd12b2de424
Johan Ullen <johan.albert.ullen@gmail.com>
2021-05-07 21:43:44 +0200

    Update testfile.py
    not working

--- testfile.py
+++ testfile.py
@@ -1,9 +1,15 @@
 import subprocess
 import logging
+import yaml
+import json
 
 def something(cronjob_path, dry_run):
     cronjob_def = yaml.full_load(open(cronjob_path))
-    SubprocessHelper.run(['kubectl', '-n', 'data-platform', 'apply', '-f', '-'], input = json.dumps(cronjob_def), encoding='utf-8', check=True)
+    cmd = ['kubectl', '-n', 'data-platform', 'apply', '-f', '-']
+    conf = json.dumps(cronjob_def)
+    logging.info(cmd)
+    logging.info(conf)
+    SubprocessHelper.run(cmd, input = json.dumps(cronjob_def), encoding='utf-8', check=True)
   
 class SubprocessHelper():
     @staticmethod
